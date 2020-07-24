07ec6d51a4dcde87531d4dda0f343d4f845c3c9b
Pratik Nayak <pratikvn@protonmail.com>
2020-07-24 23:01:33 +0200

    Distributed apply for COO and minor distributed array updates.

--- include/ginkgo/core/base/lin_op.hpp
+++ include/ginkgo/core/base/lin_op.hpp
@@ -156,7 +156,7 @@ public:
     {
         this->template log<log::Logger::linop_apply_started>(this, b, x);
         this->validate_application_parameters(b, x);
-        auto exec = this->get_executor();
+        auto exec = this->get_executor()->get_sub_executor();
         this->apply_impl(make_temporary_clone(exec, b).get(),
                          make_temporary_clone(exec, x).get());
         this->template log<log::Logger::linop_apply_completed>(this, b, x);
@@ -170,7 +170,7 @@ public:
     {
         this->template log<log::Logger::linop_apply_started>(this, b, x);
         this->validate_application_parameters(b, x);
-        auto exec = this->get_executor();
+        auto exec = this->get_executor()->get_sub_executor();
         this->apply_impl(make_temporary_clone(exec, b).get(),
                          make_temporary_clone(exec, x).get());
         this->template log<log::Logger::linop_apply_completed>(this, b, x);
@@ -193,7 +193,7 @@ public:
         this->template log<log::Logger::linop_advanced_apply_started>(
             this, alpha, b, beta, x);
         this->validate_application_parameters(alpha, b, beta, x);
-        auto exec = this->get_executor();
+        auto exec = this->get_executor()->get_sub_executor();
         this->apply_impl(make_temporary_clone(exec, alpha).get(),
                          make_temporary_clone(exec, b).get(),
                          make_temporary_clone(exec, beta).get(),
@@ -212,7 +212,7 @@ public:
         this->template log<log::Logger::linop_advanced_apply_started>(
             this, alpha, b, beta, x);
         this->validate_application_parameters(alpha, b, beta, x);
-        auto exec = this->get_executor();
+        auto exec = this->get_executor()->get_sub_executor();
         this->apply_impl(make_temporary_clone(exec, alpha).get(),
                          make_temporary_clone(exec, b).get(),
                          make_temporary_clone(exec, beta).get(),
